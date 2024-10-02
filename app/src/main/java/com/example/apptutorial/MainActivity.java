8ced286d97211428427974b165cc4623869c1881
David Suco <dfsuco@gmail.com>
2024-10-02 09:59:03 +0200

    Crear clases IntentImplicito y IntentExplicito. Añadir un boton con un intent para que cambie de actividad

--- app/src/main/java/com/example/apptutorial/MainActivity.java
+++ app/src/main/java/com/example/apptutorial/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.apptutorial;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.example.apptutorial.intents.IntentExplicito;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +25,27 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        //Obtener boton de la vista usando el metodo findViewById para buscar un elemento en la vista por ID
+        Button btnIntent = findViewById(R.id.buttonIntent);
+
+        //añadir un listener al boton usando el objeto de antes
+        btnIntent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /*crear intent explicito para pasar de actividad creando un objeto de la clase Intent
+                * pasando por parametros la clase en la que estamos y la axtividad a la que queremos ir*/
+                Intent intentExplicito = new Intent(MainActivity.this, IntentExplicito.class);
+
+                //Hay que lanzar el intent
+                startActivity(intentExplicito);
+
+            }
+        });
+
+
+
+
+
     }
 }
